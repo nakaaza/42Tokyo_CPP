027765a6db95a65b585f116cef96c1afe75025a0
nakaaza <nakaaza.tokyo@gmail.com>
2025-11-26 00:12:54 +0900

    CPP01 ex02

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <string>
 
 int	main()
 {
+	std::string	stringVAR = "HI THIS IS BRAIN";
+	std::string *stringPTR = &stringVAR;
+	std::string &stringREF = stringVAR;
+
+	std::cout << "&stringVAR:\t" << &stringVAR << std::endl;
+	std::cout << "stringPTR:\t" << stringPTR << std::endl;
+	std::cout << "&stringREF:\t" << &stringREF << std::endl;
+	std::cout << "stringVAR:\t" << stringVAR << std::endl;
+	std::cout << "*stringPTR:\t" << *stringPTR << std::endl;
+	std::cout << "stringREF:\t" << stringREF << std::endl;
 	return (0);
 }
