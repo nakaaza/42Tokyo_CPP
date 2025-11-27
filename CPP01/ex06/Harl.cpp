b062dc06a3e55b115312c44f589816d04bd42b7a
nakaaza <nakaaza.tokyo@gmail.com>
2025-11-28 01:09:20 +0900

    CPP01 ex06

---
+++ CPP01/ex06/Harl.cpp
@@ -0,0 +1,59 @@
+#include "Harl.hpp"
+
+Harl::Harl(void) {}
+
+Harl::~Harl(void) {}
+
+void	Harl::complain(std::string level)
+{
+	std::string	levels[4] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+
+	switch (std::find(levels, levels + 4, level) - levels)
+	{
+		case 0:
+			std::cout << "[ DEBUG ]" << std::endl;
+			Harl::debug();
+			std::cout << std::endl;
+		case 1:
+			std::cout << "[ INFO ]" << std::endl;
+			Harl::info();
+			std::cout << std::endl;
+		case 2:
+			std::cout << "[ WARNING ]" << std::endl;
+			Harl::warning();
+			std::cout << std::endl;
+		case 3:
+			std::cout << "[ ERROR ]" << std::endl;
+			Harl::error();
+			std::cout << std::endl;
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
+}
+
+void	Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my ";
+	std::cout << "7XL-double-cheese-triple-pickle-special-ketchup burger. ";
+	std::cout << "I really do!" << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. ";
+	std::cout << "You didn't put enough bacon in my burger! ";
+	std::cout << "If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. ";
+	std::cout << "I've been coming for years, ";
+	std::cout << "whereas you started working here just last month." << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
--- CPP01/ex06/main.cpp
+++ CPP01/ex06/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "Harl.hpp"
 
-int	main()
+int	main(int argc, char **argv)
 {
+	Harl	harl;
+
+	if (argc != 2)
+	{
+		std::cerr << "Invalid argument(s): ./harlFilter <level>" << std::endl;
+		return (1);
+	}
+	harl.complain(argv[1]);
 	return (0);
 }
