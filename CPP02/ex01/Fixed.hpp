951df5f6855d88d461bc5af119bd61962a637f14
nakaaza <nakaaza.tokyo@gmail.com>
2025-12-06 13:16:00 +0900

    [TMP] CPP02 ex02: still left overflow handling and ostream overload

---
+++ CPP02/ex01/Fixed.hpp
@@ -0,0 +1,34 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <limits>
+
+# define FRAC_BITS 8
+# define FIXED_MAX std::numeric_limits<int>::max() >> FRAC_BITS
+# define FIXED_MIN std::numeric_limits<int>::min() >> FRAC_BITS
+
+class Fixed {
+
+private:
+	const static int	frac_bits;
+	int					val;
+
+public:
+	Fixed();
+	Fixed(const int ini_val);
+	Fixed(const float ini_val);
+	Fixed(const Fixed &src);
+	Fixed &operator=(const Fixed &rhs);
+	~Fixed();
+
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+	float	toFloat(void) const;
+	int		toInt(void) const;
+
+
+};
+
+// std::ostream &operator<<(std::ostream &os, const Fixed &fixed);
+
+#endif
