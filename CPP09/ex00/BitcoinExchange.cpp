3cf57c84c98131717d5a4287afb10922700aea8e
Taichi Nakaza <tnakaza@c6r4s5.42tokyo.jp>
2026-02-22 16:15:23 +0900

    CPP09 ex00

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -1,64 +1,175 @@
-#include "Span.hpp"
+#include "BitcoinExchange.hpp"
+#include <map>
+#include <string>
+#include <fstream>
+#include <sstream>
 #include <iostream>
 #include <stdexcept>
-#include <algorithm>
-#include <limits>
 
-Span::Span(const Span &src):
-	_max_size(src._max_size),
-	_data(src._data)
-{}
+t_data	BitcoinExchange::data;
 
-Span::Span(const unsigned int N): _max_size(N) {}
+void	BitcoinExchange::init_data(const std::string &filename) {
+	std::ifstream ifs(filename.c_str());
+	if (!ifs)
+		throw std::runtime_error("failed to open file: " + filename);
 
-Span &Span::operator=(const Span &rhs) {
-	if (this != &rhs) {
-		_max_size = rhs._max_size;
-		_data = rhs._data;
+	std::string line;
+
+	if (!std::getline(ifs, line))
+		return;
+
+	while (std::getline(ifs, line))
+	{
+		if (line.empty())
+			continue;
+
+		std::string date, valueStr;
+
+		std::stringstream ss(line);
+		if (!std::getline(ss, date, ','))
+			continue;
+		if (!std::getline(ss, valueStr))
+			continue;
+
+		if (date.empty() || valueStr.empty())
+			continue;
+
+		std::stringstream vs(valueStr);
+		double value;
+		vs >> value;
+
+		data[date] = value;
 	}
-	return *this;
+
+	return;
 }
 
-Span::~Span() {}
-
-void	Span::addNumber(int val)
+static bool is_valid_date(const std::string &date)
 {
-	if (_data.size() >= _max_size)
-		throw AlreadyFull();
-	_data.push_back(val);
-}
+	if (date.length() != 10)
+		return false;
 
-unsigned long	Span::shortestSpan() const
-{
-	if (_data.size() <= 1)
-		throw SpanNotFound();
-	std::vector<int> tmp = _data;
-	std::sort(tmp.begin(), tmp.end());
-	unsigned long	ss = std::numeric_limits<unsigned long>::max();
-	for (std::vector<int>::size_type i = 0; i + 1 < tmp.size(); i++) {
-		unsigned long s = tmp[i + 1] - tmp[i];
-		if (s < ss)
-			ss = s;
+	if (date[4] != '-' || date[7] != '-')
+		return false;
+
+	for (int i = 0; i < 10; ++i)
+	{
+		if (i == 4 || i == 7)
+			continue;
+		if (!std::isdigit(date[i]))
+			return false;
 	}
-	return ss;
+
+	int year, month, day;
+	std::stringstream ss(date);
+	char dash1, dash2;
+	ss >> year >> dash1 >> month >> dash2 >> day;
+
+	if (ss.fail())
+		return false;
+
+	if (month < 1 || month > 12)
+		return false;
+
+	if (day < 1 || day > 31)
+		return false;
+
+	return true;
 }
 
-unsigned long	Span::longestSpan() const
+static bool is_valid_number(const std::string &num_str, double &value)
 {
-	if (_data.size() <= 1)
-		throw SpanNotFound();
-	return *std::max_element(_data.begin(), _data.end())
-		   - *std::min_element(_data.begin(), _data.end());
+	if (num_str.empty())
+		return false;
+
+	std::stringstream ss(num_str);
+
+	ss >> value;
+
+	if (ss.fail() || !(ss >> std::ws).eof())
+		return false;
+
+	return true;
 }
 
-const char*	Span::AlreadyFull::what() const throw() {
-	return "ERROR: Couldn't add number to Span because it is already filled with _max_size numbers.";
+static bool is_valid_line(const std::string &line, std::string& date, double &val)
+{
+	std::string::size_type pos = line.find(" | ");
+	if (pos == std::string::npos) {
+		std::cerr << "ERROR: invalid input => " << line << std::endl;
+		return false;
+	}
+
+	std::string date_str = line.substr(0, pos);
+	std::string val_str = line.substr(pos + 3);
+
+	if (!is_valid_date(date_str)) {
+		std::cerr << "ERROR: invalid date => " << date_str << std::endl;
+		return false;
+	}
+
+	if (!is_valid_number(val_str, val)) {
+		std::cerr << "ERROR: invalid number => " << val_str << std::endl;
+		return false;
+	}
+
+	if (val < 0) {
+		std::cerr << "ERROR: not a positive number => " << val_str << std::endl;
+		return false;
+	}
+
+	if (val > 1000) {
+		std::cerr << "ERROR: too large a number => " << val_str << std::endl;
+		return false;
+	}
+
+	date = date_str;
+	return true;
 }
 
-const char*	Span::ExceedsCapacity::what() const throw() {
-	return "ERROR: Couldn't add numbers to Span because it would exceed capacity(_max_size).";
+void	BitcoinExchange::execute(const std::string& filename) {
+	std::ifstream ifs(filename.c_str());
+	if (!ifs)
+		throw std::runtime_error("failed to open file: " + filename);
+
+	std::string line;
+
+	if (!std::getline(ifs, line))
+		return;
+
+	while (std::getline(ifs, line))
+	{
+		if (line.empty())
+			continue;
+		std::string	date;
+		double		val;
+		if (!is_valid_line(line, date, val))
+			continue;
+
+		double	rate = get_rate(date);
+		if (rate < 0) {
+			std::cerr << "ERROR: rate not found  => " << date << std::endl;
+			continue;
+		}
+		std::cout << date << " => " << val << " = " << val * rate << std::endl;
+	}
+
+	return;
 }
 
-const char*	Span::SpanNotFound::what() const throw() {
-	return "ERROR: Couldn't find span because no numbers or only one number stored.";
+double BitcoinExchange::get_rate(const std::string& date)
+{
+	if (data.empty())
+		return -1;
+
+	t_data::iterator it = data.lower_bound(date);
+
+	if (it != data.end() && it->first == date)
+		return it->second;
+
+	if (it == data.begin())
+		return -1;
+
+	--it;
+	return it->second;
 }
--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -1,137 +1,17 @@
-#include "Span.hpp"
+#include "BitcoinExchange.hpp"
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 
-void basic_test()
+int	main(int argc, char *argv[])
 {
-	std::cout << "[Basic test] ========================" << std::endl;
-	Span sp = Span(5);
-	std::cout << "Created Span with capacity of 5 elements" << std::endl;
-	try {
-		std::cout << "Trying to execute shortestSpan() with 0 elements" << std::endl;
-		std::cout << sp.shortestSpan() << std::endl;
-		std::cout << "-> SUCCESS!" << std::endl;
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
+	if (argc != 2) {
+		std::cerr << "Error: Invalid argument(s)." << std::endl
+				  << "Usage: " << argv[0] << " <filename>" << std::endl;
+		return 1;
 	}
-	std::cout << std::endl;
 
-	try {
-		std::cout << "Trying to execute addNumber() with 0 elements" << std::endl;
-		sp.addNumber(6);
-		std::cout << "-> SUCCESS!" << std::endl;
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << std::endl;
+	std::string	db_filename = "data.csv";
+	BitcoinExchange::init_data(db_filename);
 
-	try {
-		std::cout << "Trying to execute longestSpan() with 1 element" << std::endl;
-		std::cout << sp.longestSpan() << std::endl;
-		std::cout << "-> SUCCESS!" << std::endl;
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-
-	try {
-		sp.addNumber(3);
-		sp.addNumber(17);
-		sp.addNumber(9);
-		sp.addNumber(11);
-		std::cout << "Added 5 elements to Span" << std::endl;
-		std::cout << "-> SUCCESS!" << std::endl;
-	} catch (std::exception &e) {
-		std::cout << e.what() <<std::endl;
-	}
-	std::cout << std::endl;
-
-	try {
-		std::cout << "Trying to execute shortestSpan() with 5 elements" << std::endl;
-		std::cout << sp.shortestSpan() << std::endl;
-		std::cout << "-> SUCCESS!" << std::endl;
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-
-	try {
-		std::cout << "Trying to execute longestSpan() with 5 elements" << std::endl;
-		std::cout << sp.longestSpan() << std::endl;
-		std::cout << "-> SUCCESS!" << std::endl;
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-
-	try {
-		std::cout << "Trying to execute addNumber() with 5 elements" << std::endl;
-		sp.addNumber(42);
-		std::cout << "-> SUCCESS!" << std::endl;
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-	return;
-}
-
-void	big_test()
-{
-	std::cout << "[Big test] ========================" << std::endl;
-	std::srand(std::time(NULL));
-
-	std::vector<int>::size_type size = 42000;
-	std::vector<int> v;
-	v.reserve(size);
-
-	for (std::size_t i = 0; i < size; ++i)
-		v.push_back(rand());
-
-	Span	small = Span(size / 2);
-	try {
-		std::cout << "Trying to excecute add " << size << " elements to small Span with capacity of " << size / 2 << std::endl;
-		small.addNumbers(v.begin(), v.end());
-		std::cout << "-> SUCCESS!" << std::endl;
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-
-
-	Span	big = Span(size);
-	try {
-		std::cout << "Trying to excecute add " << size << " elements to big Span with capacity of " << size << std::endl;
-		big.addNumbers(v.begin(), v.end());
-		std::cout << "-> SUCCESS!" << std::endl;
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-
-	try {
-		std::cout << "Trying to execute shortestSpan() with " << size << " elements" << std::endl;
-		std::cout << big.shortestSpan() << std::endl;
-		std::cout << "-> SUCCESS!" << std::endl;
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-
-	try {
-		std::cout << "Trying to execute longestSpan() with " << size << " elements" << std::endl;
-		std::cout << big.longestSpan() << std::endl;
-		std::cout << "-> SUCCESS!" << std::endl;
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-}
-
-
-int	main(void)
-{
-	basic_test();
-	std::cout << std::endl;
-	big_test();
+	std::string	input_filename = argv[1];
+	BitcoinExchange::execute(input_filename);
 }
